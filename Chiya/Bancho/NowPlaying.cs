2d8761f6a04a86e1be616bb80bf165750dcc3bd5
ilsubyeega <ilsubyeega@naver.com>
2020-11-26 10:31:26 +0900

    Reworked np parser.

---
+++ Chiya/Bancho/NowPlaying.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chiya.Bancho
+{
+	public enum NowPlayingType
+	{
+		NULL,
+		LISTENING,
+		PLAYING,
+		WATCHING
+	}
+	public static class NowPlayingUtil
+	{
+		public static string GetSplitValue(this NowPlayingType type)
+		{
+			switch (type)
+			{
+				default:
+					throw new Exception("No Type of this now playing value.");
+				case NowPlayingType.LISTENING:
+					return "is listening to";
+				case NowPlayingType.PLAYING:
+					return "is playing";
+				case NowPlayingType.WATCHING:
+					return "is watching";
+			}
+		}
+	}
+}
--- Chiya/Bancho/NowPlayingParser.cs
+++ Chiya/Bancho/NowPlayingParser.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Internal;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Chiya.Bancho
 {
@@ -16,37 +18,94 @@ namespace Chiya.Bancho
 		public static NowPlaying Parse(string text)
 		{
 			NowPlaying np = new NowPlaying();
-			if (text.StartsWith("\u0001ACTION is listening to"))
-			{
-				np.Url = text.Split("\u0001ACTION is listening to [")[1].Split(" ")[0];
-			} else if (text.StartsWith("\u0001ACTION is playing"))
-			{
-				np.Url = text.Split("\u0001ACTION is playing [")[1].Split(" ")[0];
-			}
-			
-			np.Beatmap = text.Split(np.Url)[1].Substring(1).Split("]\u0001")[0];
-			if (np.Url.Contains("https://osu.ppy.sh/b/"))
-			{
-				np.Id = int.Parse(np.Url.Split("https://osu.ppy.sh/b/")[1]);
-				np.IsMapSet = false;
-			}
-			else if (np.Url.Contains("https://osu.ppy.sh/s/"))
-			{
-				np.Id = int.Parse(np.Url.Split("https://osu.ppy.sh/s/")[1]);
-				np.IsMapSet = true;
-			}
-			else
-			{
-				np.Id = 0; // null
-			}
+			GetSongId(np, text);
+			var mods = GetMods(text);
+			var mode = GetMode(text);
+			np.Mods = mods;
+			np.Mode = mode;
 			return np;
+		}
+		private static NowPlayingType GetNpType(string text)
+		{
+			if (text.StartsWith("\u0001ACTION is listening to "))
+			{
+				return NowPlayingType.LISTENING;
+			} else if (text.StartsWith("\u0001ACTION is playing "))
+			{
+				return NowPlayingType.PLAYING;
+			} else if (text.StartsWith("\u0001ACTION is watching "))
+			{
+				return NowPlayingType.WATCHING;
+			}
+			return NowPlayingType.NULL;
+		}
+		public static void GetSongId(NowPlaying np, string text)
+		{
+			var tmp = text.Split("\u0001ACTION ")[1].Split(GetNpType(text).GetSplitValue() + " [")[1].Split(" ")[0];
+			if (tmp.StartsWith("https://osu.ppy.sh/b/"))
+			{
+				np.IsMapSet = false;
+				np.Id = int.Parse(tmp.Split("https://osu.ppy.sh/b/")[1]);
+			} else if (tmp.StartsWith("https://osu.ppy.sh/s/")){
+				np.IsMapSet = true;
+				np.Id = int.Parse(tmp.Split("https://osu.ppy.sh/s/")[1]);
+			} else
+			{
+				throw new Exception("Cannot parse url for id: " + tmp);
+			}
+		}
+		public static string[] GetMods(string text)
+		{
+			var tmp = text.Split("\u0001ACTION ")[1].Split(GetNpType(text).GetSplitValue() + " [")[1].Split(" ").Skip(1).Join(" ").Split("] ");
+			var tmp_2 = tmp[tmp.Length-1].Split("\u0001")[0].Split("+");
+			List<String> value = new List<string>();
+			foreach (string _t in tmp_2)
+			{
+				string _tmp = null;
+				if (_t.Length <= 1) // dont add null.
+					continue;
+				if (_t.EndsWith(" ")) // "Hidden "
+					_tmp = _t.Substring(0, _t.IndexOf(" "));
+				if (_t.StartsWith("<")) // <osu!mania>
+					continue;
+				else if (_t.Contains(" |")) // DoubleTime |7K|
+				{
+					var tmp2 = _t.Split(" ");
+					
+					if (tmp2.Length >= 1)
+					{
+						value.Add(tmp2[0]);
+						_tmp = tmp2[1].Replace("|", "").Replace("|", ""); // Add external value, if mania.
+					} else
+					{
+						_tmp = tmp2[0];
+					}
+				} else
+				{
+					if (_tmp == null || _tmp.Length <= 1)
+						_tmp = _t;
+				}
+				
+				value.Add(_tmp);
+			}
+			return value.ToArray();
+		}
+		public static short GetMode(string text)
+		{
+			if (text.Contains(" <Taiko> ")) return 1;
+			if (text.Contains(" <CatchTheBeat> ")) return 2;
+			if (text.Contains(" <osu!mania> ")) return 3;
+			return 0;
 		}
 	}
 	public class NowPlaying
 	{
+		public short Type = 0; // 0: Std, 1: Taiko, 2: Catch, 3: Mania
 		public string Url;
 		public string Beatmap;
 		public bool IsMapSet; // if it is Mapset 1, if it is Map 0.
 		public int Id;
+		public string[] Mods = null;
+		public short Mode = 0;
 	}
 }
